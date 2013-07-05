c85e14e6555e7109e45c594847193bef5534f8fc
DiMoN1993 <dimakovalenko1993@gmail.com>
2013-07-05 18:22:41 +0700

    "stable version of bypass dyrected cyclic graph with tests"

--- Node_checkout.php
+++ Node_checkout.php
@@ -7,93 +7,10 @@
  * To change this template use File | Settings | File Templates.
  */
 
-class Node_checkout
-{
-
-private $AllNodes = array();
-private $SwitchedNodes = array();
-private $PopOutArray;
-private $node = array();
-private $Graph = array();
-private $TestValue;
-
-function __construct($graph, $test_value)
-{
-    $this->Graph = $graph;
-    $this->TestValue = $test_value;
-}
-/**
-*@ignored
-*/
-function GetNodeArray()
-{
-    /* foreach($graph as $key_node=>$node_content)
-         {
-             if($key_node === $test_value)
-             {
-                 $this->AllNodes[] = $node_content;
-                 $this->SwitchedNodes[] = $node_content;
-
-                 while(count($this->SwitchedNodes) != 0)
-                 {
-                     $ProcessedNodeValue = array_pop($this->SwitchedNodes);
-
-                     foreach($ProcessedNodeValue as $keys=>$values)
-                     {
-
-                         if ($keys == "uuid")
-                         {
-
-                             foreach($graph as $node_content_recursion)
-                             {
-                                 $this->RecursiveArray = $node_content_recursion;
-
-                                 foreach($this->RecursiveArray as $key_for_search_references=>$point_for_search_references)
-                                 {
-
-                                     foreach($graph as $key_node)
-                                     {
-                                         if(($node_content_recursion == $key_node) and ((strpos($key_for_search_references, "_ref")) != false) and ($point_for_search_references == $values))
-                                         {
-                                             $AllNodes[] = $node_content_recursion;
-                                             $SwitchedNodes[] = $node_content_recursion;
-                                         }
-                                     }
-                                 }
-                             }
-                         }
-                     }
-                 }
-             }
-         }*/
-
-    $this->node[] = $this->Graph[$this->TestValue];
-    $this->AllNodes[$this->TestValue] = $this->node;
-    $this->SwitchedNodes[$this->TestValue] = $this->node;
-
-    $this->PopOutArray = array_pop($this->SwitchedNodes);
-
-    $Node = new Node($this->PopOutArray);
-    $dependences = $Node->getDependences();
-
-    foreach($dependences as $key=>$references)
-    {
-        $this->node[] = $this->Graph;
-        $this->PopOutArray = array_pop($this->node);
-
-        if ($this->PopOutArray = $references)
-        {
-            $this->AllNodes[] = $this->node;
-        }
-    }
-
-    return $this->AllNodes;
-}
-}
-
 class Graph
 {
     private $GraphDescription;
+
     public function __construct($Graph)
     {
         $this->GraphDescription = $Graph;
@@ -101,36 +18,41 @@ class Graph
 
     public function getSubgraph($KeyOfNode)
     {
-        $ReturnSubgraph[] = $this->GraphDescription[$KeyOfNode];
-        $node = new Node($this->GraphDescription[$KeyOfNode]);
-        $subgraphNodes = $node->getDependences();
+        $AllNodes = array();
+        $SwitchedNodes = array();
+        $AllNodes[$KeyOfNode] = $this->GraphDescription[$KeyOfNode];
+        $SwitchedNodes[$KeyOfNode] = $this->GraphDescription[$KeyOfNode];
         $i;
-        foreach($this->GraphDescription as $GraphNode)
+        while (count($SwitchedNodes) != 0)
         {
-            foreach($GraphNode as $KeyGraphNode=>$ValueGraphNode)
+            $PopOutArray = array_pop($SwitchedNodes);
+            $node = new Node($PopOutArray);
+            $subgraphNodes = $node->getDependences();
+            foreach ($subgraphNodes as $node_id)
             {
-                for($i=0; $i<count($subgraphNodes); $i++)
+                if(!isset($AllNodes[$node_id]))
                 {
-                    if(($KeyGraphNode == 'uuid') && ($ValueGraphNode == $subgraphNodes[$i]))
-                    {
-                        $ReturnSubgraph[] = $GraphNode;
-                    }
+                    $GraphNode = $this->getNode($node_id);
+                    $AllNodes[$GraphNode['uuid']] = $GraphNode;
+                    $SwitchedNodes[$GraphNode['uuid']] = $GraphNode;
                 }
             }
         }
-        return new Graph($ReturnSubgraph);
+        return new Graph($AllNodes);
+    }
+
+
+    function getNode($node_id)
+    {
+       return $this->GraphDescription[$node_id];
     }
 
     function getAllNodes()
     {
-        foreach($this->GraphDescription as $node)
-        {
-            $Node[] = $node;
-        }
-
-        return $Node;
+        return $this->GraphDescription;
     }
 }
+
 class Node
 {
     private $Node = array();
@@ -143,9 +65,9 @@ class Node
     public function getDependences()
     {
         $ReturnNode = array();
-        foreach($this->Node as $key=>$value)
+        foreach ($this->Node as $key => $value)
         {
-            if(preg_match('/_ref|_ref.[0-9]|ref/', $key))
+            if (preg_match('/_ref|_ref.[0-9]|ref/', $key))
             {
                 $ReturnNode[] = $value;
             }
--- Node_checkoutTest.php
+++ Node_checkoutTest.php
@@ -12,115 +12,10 @@ require_once('Node_checkout.php');
 
 class Node_checkoutTest extends PHPUnit_Framework_TestCase
 {
-    private $_graph_description=array(
-        'A'=>array(
-            'name'=>"Dmitri",
-            'uuid'=>"u1",
-            'first_ref'=>"u2",
-            'second_ref'=>"u5"
-        ),
-        'B'=>array(
-            'name'=>"Igor",
-            'uuid'=>"u2",
-            'first_ref'=>"u1",
-            'second_ref'=>"u4"
-        ),
-        'C'=>array(
-            'name'=>"Aleksandr",
-            'uuid'=>"u3",
-            'first_ref'=>"u4",
-            'second_ref'=>"u5"
-        ),
-        'D'=>array(
-            'name'=>"Max",
-            'uuid'=>"u4",
-            'first_ref'=>"u1"
-        ),
-        'E'=>array(
-            'name'=>"Pasha",
-            'uuid'=>"u5",
-            'first_ref'=>"u1"
-        ),
-        'K'=>array(
-            'name'=>"Sergei",
-            'uuid'=>"u6",
-            'first_ref'=>"u3"
-        )
-    );
-    private $_graph_description_required_data = array("u1", "u2", "u3", "u4", "u5");
-    private $_test_value = "D";
-    private $_set;
-    private $_return_value;
-    private $_test_array = array();
-    private $_graph_description_lvl_first = array(
-        "A"=>array("uuid"=>"1", "ref1"=>"2"),
-        "B"=>array("uuid"=>"2"));
-    private $_graph_description_lvl_second = array(
-        "A"=>array("uuid"=>"1", "ref1"=>"2"),
-        "B"=>array("uuid"=>"2", "ref1"=>"3"),
-        "C"=>array("uuid"=>"3"));
-    private $_graph_description_required_data_lvl_first = array("1", "2");
-
-    public function SetNodeData($DescriptionNode, $TestValue)
-    {
-        $Description = new Node_checkout($DescriptionNode, $TestValue);
-        $DescriptionReturn = $Description->GetNodeArray();
-        return $DescriptionReturn;
-    }
-
-    private function ReturnArrayUUid($DescriptionOfArray)
-    {
-        foreach($DescriptionOfArray as $Node)
-        {
-            foreach($Node as $key=>$Node_value)
-            {
-                if($key == "uuid") $this->_test_array[]=$Node_value;
-            }
-        }
-        return $this->_test_array;
-    }
-
-    public function testSubgraph()
-    {
-        $graph = new Graph($this->_graph_description_lvl_first);
-        $subgraph = $graph->getSubgraph('A');
-        $SetNodeData = $subgraph->getAllNodes();
-
-        $this->assertInternalType("array", $SetNodeData);
-        $this->assertEquals(2, count($SetNodeData));
-
-        $this->assertEquals($this->_graph_description_lvl_first["A"], $SetNodeData[0]);
-        $this->assertEquals($this->_graph_description_lvl_first["B"], $SetNodeData[1]);
-    }
-
-    public function testSubgraphExtended()
-    {
-        $graph = new Graph($this->_graph_description_lvl_second);
-        $subgraph = $graph->getSubgraph('A');
-        $SetNodeData = $subgraph->getAllNodes();
-
-        $this->assertInternalType("array", $SetNodeData);
-        $this->assertEquals(3, count($SetNodeData));
-
-        $this->assertEquals($this->_graph_description_lvl_first["A"], $SetNodeData[0]);
-        $this->assertEquals($this->_graph_description_lvl_first["B"], $SetNodeData[1]);
-        $this->assertEquals($this->_graph_description_lvl_first["C"], $SetNodeData[2]);
-    }
-
-    public function testArrayReturnValueLvlFirst()
-    {
-        $graph = new Graph($this->_graph_description_lvl_first);
-        $SetNodeData = $graph->getAllNodes();
-        $this->assertInternalType("array", $SetNodeData);
-        $this->assertEquals(2, count($SetNodeData));
-
-        $this->assertEquals($this->_graph_description_lvl_first["A"], $SetNodeData[0]);
-        $this->assertEquals($this->_graph_description_lvl_first["B"], $SetNodeData[1]);
-    }
-
     public function testNodeDependences()
     {
         $node = new Node(array('uuid' => 'uuid1', 'field_ref' => 12));
+
         $dependences = $node->getDependences();
         $this->assertEquals(12, $dependences[0]);
         $this->assertEquals(1, count($dependences));
@@ -129,9 +24,118 @@ class Node_checkoutTest extends PHPUnit_Framework_TestCase
     public function testNodeDependensesSecond()
     {
         $node = new Node(array('uuid' => 'uuid1', 'field_ref1' => 1, 'field_ref2' => 4));
+
         $dependenses = $node->getDependences();
         $this->assertEquals(1, $dependenses[0]);
         $this->assertEquals(4, $dependenses[1]);
         $this->assertEquals(2, count($dependenses));
+    }
+
+    public function testArrayReturnValueLvlFirst()
+    {
+        $array = array(
+            "1" => array("uuid" => "1", "ref1" => "2"),
+            "2" => array("uuid" => "2"));
+
+        $graph = new Graph($array);
+        $SetNodeData = $graph->getAllNodes();
+        $this->assertInternalType("array", $SetNodeData);
+        $this->assertEquals(2, count($SetNodeData));
+
+        $this->assertEquals($array["1"], $SetNodeData["1"]);
+        $this->assertEquals($array["2"], $SetNodeData["2"]);
+    }
+
+    public function testSubgraph()
+    {
+        $array = array(
+            "1" => array("uuid" => "1", "ref1" => "2"),
+            "2" => array("uuid" => "2"));
+
+        $graph = new Graph($array);
+
+        $subgraph = $graph->getSubgraph('1');
+        $SetNodeData = $subgraph->getAllNodes();
+
+        $this->assertInternalType("array", $SetNodeData);
+        $this->assertEquals(2, count($SetNodeData));
+
+        $this->assertEquals($array["1"], $SetNodeData[1]);
+        $this->assertEquals($array["2"], $SetNodeData[2]);
+    }
+
+    public function testGetNode()
+    {
+        $array = array(
+            "1" => array("uuid" => "1", "ref1" => "2"),
+            "2" => array("uuid" => "2"));
+
+        $graph = new Graph($array);
+
+        $node = $graph->getNode("2");
+
+        $this->assertEquals($array["2"], $node);
+    }
+
+    public function testSubgraphExtended()
+    {
+        $array = array(
+            "1" => array("uuid" => "1", "ref1" => "2"),
+            "2" => array("uuid" => "2", "ref1" => "3"),
+            "3" => array("uuid" => "3"));
+
+        $graph = new Graph($array);
+        $subgraph = $graph->getSubgraph('1');
+        $SetNodeData = $subgraph->getAllNodes();
+
+        $this->assertInternalType("array", $SetNodeData);
+        $this->assertEquals(3, count($SetNodeData));
+
+        $this->assertEquals($array["1"], $SetNodeData[1]);
+        $this->assertEquals($array["2"], $SetNodeData[2]);
+        $this->assertEquals($array["3"], $SetNodeData[3]);
+    }
+
+    public function testSubgraphCyclic()
+    {
+        $array = array(
+            "1" => array("uuid" => "1", "ref1" => "2"),
+            "2" => array("uuid" => "2", "ref1" => "3"),
+            "3" => array("uuid" => "3", "ref1" => "2"));
+
+        $graph = new Graph($array);
+        $subgraph = $graph->getSubgraph('1');
+        $SetNodeData = $subgraph->getAllNodes();
+
+        $this->assertInternalType("array", $SetNodeData);
+        $this->assertEquals(3, count($SetNodeData));
+
+        $this->assertEquals($array["1"], $SetNodeData[1]);
+        $this->assertEquals($array["2"], $SetNodeData[2]);
+        $this->assertEquals($array["3"], $SetNodeData[3]);
+    }
+
+    public function testFinalGraph()
+    {
+        $array = array(
+            "1" => array("uuid" => "1", "ref1" => "2"),
+            "2" => array("uuid" => "2", "ref1" => "3", "ref2" => "4"),
+            "3" => array("uuid" => "3", "ref1" => "2", "ref2" => "1"),
+            "4" => array("uuid" => "4", "ref1" => "5", "ref2" => "1"),
+            "5" => array("uuid" => "5", "ref1" => "2"),
+            "6" => array("uuid" => "6", "ref1" => "4"));
+
+        $graph = new Graph($array);
+        $subgraph = $graph->getSubgraph('1');
+        $SetNodeData = $subgraph->getAllNodes();
+
+        $this->assertInternalType("array", $SetNodeData);
+        $this->assertEquals(5, count($SetNodeData));
+
+        $this->assertEquals($array["1"], $SetNodeData[1]);
+        $this->assertEquals($array["2"], $SetNodeData[2]);
+        $this->assertEquals($array["3"], $SetNodeData[3]);
+        $this->assertEquals($array["4"], $SetNodeData[4]);
+        $this->assertEquals($array["5"], $SetNodeData[5]);
     }
 }
